ab45f2480cd438ec1ba8e6b18fc48817de186fb5
Molonlabee <janayahernandez@yahoo.com>
2020-03-15 21:39:50 -0500

    first

--- closures.js
+++ closures.js
@@ -23,13 +23,13 @@ function outer() {
 */
   
 // Code Here
-
+let inner = outer;
 
 
 //Once you do that, invoke inner.
 
 //Code Here
-
+inner();
 
 
 ////////// PROBLEM 2 //////////
@@ -52,8 +52,8 @@ function callFriend(name) {
 */
 
 //Code Here
-
-
+let callJake = callFriend('Jake');
+callJake('435-555-9248')
 
 ////////// PROBLEM 3 //////////
 
@@ -62,15 +62,20 @@ function callFriend(name) {
 */
 
 //Code Here
-
+function makeCounter() {
+  let num = 0;
+  return function makeCounter2() {
+    return ++num;
+  }
+}
 
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+  var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -87,9 +92,15 @@ function callFriend(name) {
 
 function counterFactory(value) {
   // Code here.
-
+  function inc(){
+    return ++value
+  }
+  function dec(){
+    return --value
+  }
   return {
-
+    inc,
+    dec
   };
 }
 
@@ -113,9 +124,12 @@ function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
 
   // code message function here.
+  function message(){
+    return `${welcomeText} ${firstname} ${lastname}.`
+    //Uncommment this to return the value of your message function
+  }
+  return message;
 
-  //Uncommment this to return the value of your message function
-  //return message;
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
@@ -144,10 +158,13 @@ var module = (function() {
   // outside our lexical scope
   return {
     // Code here.
+    publicMethod: function(){
+      return privateMethod();
+    }
   };
-})();
-
-
+}
+)()
+module.publicMethod();
 
 ////////// PROBLEM 7 //////////
 
@@ -163,12 +180,19 @@ function secretNumber() {
 
   return {
     // Code here
+    addToSecret: function(num){
+      return secret += num
+    },
+    takeAwayFromSecret: function(num){
+      return secret -= num
+    }
   };
 }
 
 
 
 ////////// PROBLEM 8 //////////
+//extra
   
 /*
   Here we have a for loop that will iterate as long as i is less than or equal to 5.
@@ -186,11 +210,14 @@ function secretNumber() {
   Fix the code below to log the desired output.
 */
 
-function timeOutCounter() {
-  for (var i = 0; i <= 5; i++) {
-    setTimeout(function() {
-      console.log(i);
-    }, i * 1000);
+function timeOutCounter(){
+  for(var i = 0; i <=5; i++){
+    function runTimeout(currVal){
+      setTimeout(function() {
+        console.log(currVal)
+      }, currVal * 1000)
+    }
+    runTimeout(i)
   }
 }
-timeOutCounter();
+timeOutCounter()
